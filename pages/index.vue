cd063e0dcc64204cfa33d76dd8f36ea1f4285cfe
Equan Pr <equan.p@gmail.com>
2019-08-18 11:02:26 +0700

    style untuk link quote

--- pages/index.vue
+++ pages/index.vue
@@ -6,7 +6,7 @@
           <p class="text-quote">
           "Dengan terhafal dalam bentuk nyanyian, kalender Jawa akan terbawa kemana saja pergi, dan masih memiliki tingkat akurasi tinggi sampai 994 tahun lagi. Setelah itu baru ada koreksi +1 hari sebelum Ambal Kurup Alip Senen Kliwon, 1 Sura 2947 Jawa (26 Januari 2984 Masehi)"
           </p>
-          <p class="subtitle">
+          <p class="text-quote-contrib">
             - caknun.com
           </p>
         </div>
