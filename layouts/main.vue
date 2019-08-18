e5f27173f371a774ff9e64e8389d3905dbab81b2
Equan Pr <equan.p@gmail.com>
2019-08-18 11:02:41 +0700

    format

--- layouts/main.vue
+++ layouts/main.vue
@@ -11,11 +11,15 @@
     </nav>
     <div class="container">
       <div class="section">
-        <h1 class="text-logo">pustaka</h1>
-        <h1 class="text-logo">kalender jawa</h1>
-        <p
-          class="text-deskripsi"
-        >Pustaka JavaScript untuk perhitungan, pengkonversian dan penanggalan abadi Kalender Jawa</p>
+        <h1 class="text-logo">
+          pustaka
+        </h1>
+        <h1 class="text-logo">
+          kalender jawa
+        </h1>
+        <p class="text-deskripsi">
+          Pustaka JavaScript untuk perhitungan, pengkonversian dan penanggalan abadi Kalender Jawa
+        </p>
       </div>
       <nuxt />
     </div>
