3fad621e6ee7b1d3ebac53bd479ce47638041f81
Equan Pr <equan.p@gmail.com>
2019-08-18 10:40:05 +0700

    3 menu navigasi

--- layouts/main.vue
+++ layouts/main.vue
@@ -1,16 +1,23 @@
 <template>
-  <div class="container">
-    <div class="section">
-      <h1 class="text-logo">
-        pustaka
-      </h1>
-      <h1 class="text-logo">
-        kalender jawa
-      </h1>
-      <p class="text-deskripsi">
-        Pustaka JavaScript untuk perhitungan, pengkonversian dan penanggalan abadi Kalender Jawa
-      </p>
+  <div>
+    <nav class="navbar">
+      <div class="navbar-menu is-active">
+        <div class="navbar-end">
+          <a class="navbar-item">API</a>
+          <a class="navbar-item">Dokumentasi</a>
+          <a class="navbar-item">Github</a>
+        </div>
+      </div>
+    </nav>
+    <div class="container">
+      <div class="section">
+        <h1 class="text-logo">pustaka</h1>
+        <h1 class="text-logo">kalender jawa</h1>
+        <p
+          class="text-deskripsi"
+        >Pustaka JavaScript untuk perhitungan, pengkonversian dan penanggalan abadi Kalender Jawa</p>
+      </div>
+      <nuxt />
     </div>
-    <nuxt />
   </div>
 </template>
