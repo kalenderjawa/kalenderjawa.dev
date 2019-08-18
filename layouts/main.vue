314c186a0b5ae12cbb6e38e7e7aac80dc17d5edd
Equan Pr <equan.p@gmail.com>
2019-08-18 08:22:52 +0700

    tidak memakai navbar tapi seksi

--- layouts/main.vue
+++ layouts/main.vue
@@ -1,12 +1,13 @@
 <template>
-  <div>
-    <nav class="navbar" role="navigation">
-      <div class="navbar-brand">
-        <h1 class="text-logo">
-          pustaka kalender jawa
-        </h1>
-      </div>
-    </nav>
+  <div class="container">
+    <div class="section">
+      <h1 class="text-logo">
+        pustaka kalender jawa
+      </h1>
+      <p class="text-deskripsi">
+        Pustaka JavaScript untuk perhitungan, pengkonversian dan penanggalan abadi Kalender Jawa
+      </p>
+    </div>
     <nuxt />
   </div>
 </template>
