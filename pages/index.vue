d3979c6dc45bd8358122a3728df00f0daa83c0ee
Equan Pr <equan.p@gmail.com>
2019-08-18 10:39:04 +0700

    quote diletakkan dalam card

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,16 @@
 <template>
   <div>
     <section class="section">
-      <dir class="content">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum exercitationem, suscipit totam porro eum illo nam quis dolore voluptas autem unde corrupti asperiores facilis quisquam doloribus blanditiis eveniet nesciunt cumque!
-      </dir>
+      <div class="card">
+        <div class="card-content">
+          <p class="text-quote">
+          "Dengan terhafal dalam bentuk nyanyian, kalender Jawa akan terbawa kemana saja pergi, dan masih memiliki tingkat akurasi tinggi sampai 994 tahun lagi. Setelah itu baru ada koreksi +1 hari sebelum Ambal Kurup Alip Senen Kliwon, 1 Sura 2947 Jawa (26 Januari 2984 Masehi)"
+          </p>
+          <p class="subtitle">
+            - caknun.com
+          </p>
+        </div>
+      </div>
     </section>
   </div>
 </template>
