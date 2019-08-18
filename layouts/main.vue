2b5daec09989012521b905e9c8202e677a56c5d9
Equan Pr <equan.p@gmail.com>
2019-08-18 07:52:00 +0700

    logo text

--- layouts/main.vue
+++ layouts/main.vue
@@ -1,5 +1,12 @@
 <template>
   <div>
+    <nav class="navbar" role="navigation">
+      <div class="navbar-brand">
+        <h1 class="text-logo">
+          pustaka kalender jawa
+        </h1>
+      </div>
+    </nav>
     <nuxt />
   </div>
 </template>
