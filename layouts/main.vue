b531093edd1e8afd6008e8e163da71669f49e892
Equan Pr <equan.p@gmail.com>
2019-08-18 09:50:29 +0700

    pisahkan logo text menjadi dua line

--- layouts/main.vue
+++ layouts/main.vue
@@ -2,7 +2,10 @@
   <div class="container">
     <div class="section">
       <h1 class="text-logo">
-        pustaka kalender jawa
+        pustaka
+      </h1>
+      <h1 class="text-logo">
+        kalender jawa
       </h1>
       <p class="text-deskripsi">
         Pustaka JavaScript untuk perhitungan, pengkonversian dan penanggalan abadi Kalender Jawa
