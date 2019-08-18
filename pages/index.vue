8e9a02c12be29527e30289d1310e42db6f2ba88c
Equan Pr <equan.p@gmail.com>
2019-08-18 07:52:35 +0700

    sample paragraf

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,10 @@
 <template>
-  <div class="container">
-    <h1>Dev</h1>
+  <div>
+    <section class="section">
+      <dir class="content">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum exercitationem, suscipit totam porro eum illo nam quis dolore voluptas autem unde corrupti asperiores facilis quisquam doloribus blanditiis eveniet nesciunt cumque!
+      </dir>
+    </section>
   </div>
 </template>
 
