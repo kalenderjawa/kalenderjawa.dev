4184a7298a8573bd5ff6879cd6e9c9acd91aefc6
Equan Pr <equan.p@gmail.com>
2019-08-20 13:00:34 +0700

    Ubah font-size, color supaya lebih konsisten

--- pages/index.vue
+++ pages/index.vue
@@ -20,7 +20,7 @@
               <span class="icon is-large"><i class="fab fa-github"></i></span>
               OPEN SOURCE
             </p>
-            <p>Pustaka ini bersifat sumber terbuka artinya anda bebas melihat kode program sehingga andapun juga bebas untuk menambah fitur maupun mengubahnya menjadi lebih baik.</p>
+            <p class="fitur-deskripsi">Pustaka ini bersifat sumber terbuka artinya anda bebas melihat kode program sehingga andapun juga bebas untuk menambah fitur maupun mengubahnya menjadi lebih baik.</p>
           </div>
         </div>
         <div class="tile is-6 is-parent middle-right-section">
@@ -29,7 +29,7 @@
               <span class="icon is-large"><i class="fas fa-infinity"></i></span>
               RUMUS ABADI
             </p>
-            <p>Pustaka ini memakai rumus perhitungan abadi dan rumus matematis ini dapat anda dapatkan dengan mudah melalui penggunaan API yang sederhana.</p>
+            <p class="fitur-deskripsi">Pustaka ini memakai rumus perhitungan abadi dan rumus matematis ini dapat anda dapatkan dengan mudah melalui penggunaan API yang sederhana.</p>
           </div>
         </div>
       </div>
@@ -42,7 +42,7 @@
               <span class="icon is-large"><i class="fab fa-js-square"></i></span>
               JAVASCRIPT
             </p>
-            <p>Pustaka ini tersedia melalui browser maupun npm sehingga anda bisa memakainya dalam membangun berbagai project web, desktop maupun aplikasi mobile.</p>
+            <p class="fitur-deskripsi">Pustaka ini tersedia melalui browser maupun npm sehingga anda bisa memakainya dalam membangun berbagai project web, desktop maupun aplikasi mobile.</p>
           </div>
         </div>
         <div class="tile is-6 is-parent middle-right-section">
@@ -51,7 +51,7 @@
               <span class="icon is-large"><i class="fab fa-discord"></i></span>
               RAMAH PENGEMBANG
             </p>
-            <p>Pustaka ini menyediakan API & dokumentasi yang lengkap sehingga mudah untuk digunakan, untuk mengenal lebih jauh silahkan ngobrol ke saluran Discord kami.</p>
+            <p class="fitur-deskripsi">Pustaka ini menyediakan API & dokumentasi yang lengkap sehingga mudah untuk digunakan, untuk mengenal lebih jauh silahkan ngobrol ke saluran Discord kami.</p>
           </div>
         </div>
       </div>
