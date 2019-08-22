ee7745065dc1de27679a7e4398d148e26b715bb4
Equan Pr <equan.p@gmail.com>
2019-08-22 19:06:06 +0700

    Komponen demo untuk pencarian hari dan pasaran awal bulan di tahun jawa

---
+++ components/HPAwalBulan.vue
@@ -0,0 +1,45 @@
+
+<template>
+  <div class="columns">
+    <div class="column">
+      <div class="field">
+      <label class="label">Bulan</label>
+      <div class="control">
+        <div class="select">
+          <select v-model="selected">
+            <option v-for="prop in props" v-bind:value="prop.urutan" v-bind:key="prop.urutan">{{ prop.wulan }}</option>
+          </select>
+       </div>
+      </div>
+    </div>
+    <div class="field">
+      <label class="label">Tahun</label>
+      <div class="control">
+       <input class="input" type="text" placeholder="Tahun Jawa">
+      </div>
+    </div>
+    </div>
+    <div class="column"></div>
+  </div>
+</template>
+
+<script>
+import * as KalenderJawa from '@junwatu/kalender-jawa'
+
+export default {
+  name: 'HPAwalBulan',
+  data () {
+    return {
+      props: KalenderJawa.araningSasi,
+      selected: 1
+    }
+  },
+  methods: {
+    cariHP () {
+      KalenderJawa.cariHariPasaranAwalBulanTahunJawa('mukarom', 1953).then(function (d) {
+        // console.log(d)
+      })
+    }
+  }
+}
+</script>
