da9766ec5144c8949110b4440ec26b95b8fdf2f9
Equan Pr <equan.p@gmail.com>
2019-08-22 19:07:42 +0700

    Pke komponen HPAwalBulan dan tulis nama API pertama

--- pages/api.vue
+++ pages/api.vue
@@ -1,10 +1,11 @@
 <template>
- <div class="container api-container">
-   <section class="section">
-     <div class="content">
-       <h1 class="title is-medium">Mencari Hari dan Pasaran Awal Bulan Tahun Jawa</h1>
-       <p>Fungsi ini mencari hari dan pasaran untuk awal bulan tahun jawa</p>
-       <pre v-highlightjs>
+  <div class="container api-container">
+    <section class="section">
+      <div class="content">
+        <h1 class="title is-medium">Mencari Hari dan Pasaran Awal Bulan Tahun Jawa</h1>
+        <span class="tag is-light">cariHariPasaranAwalBulanTahunJawa(w,t)</span>
+        <p class="code-deskripsi">Fungsi ini mencari hari dan pasaran untuk awal bulan tahun jawa</p>
+        <pre v-highlightjs>
          <code class="javascript">
 import * as KalenderJawa from '@junwatu/kalender-jawa'
 
@@ -13,23 +14,23 @@ KalenderJawa.cariHariPasaranAwalBulanTahunJawa('romadon', 1952).then({kH, kP} =>
 })
          </code>
        </pre>
-     </div>
-   </section>
- </div>
+       <HPAwalBulan/>
+      </div>
+    </section>
+  </div>
 </template>
 
 <script>
 import Vue from 'vue'
 import VueHighlightJS from 'vue-highlightjs'
-import * as KalenderJawa from '@junwatu/kalender-jawa'
+import HPAwalBulan from '~/components/HPAwalBulan.vue'
 
 Vue.use(VueHighlightJS)
 
-KalenderJawa.cariHariPasaranAwalBulanTahunJawa('mukarom', 1953).then(function (d) {
-  console.log(d)
-})
-
 export default {
-  layout: 'tambahan'
+  layout: 'tambahan',
+  components: {
+    HPAwalBulan
+  }
 }
 </script>
