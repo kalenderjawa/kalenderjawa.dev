786dfd9ac77c9d8717a1a16f874d3e1b320e5e85
Equan Pr <equan.p@gmail.com>
2019-08-24 19:04:13 +0700

    Pindahkan isi API pertama ke component

--- pages/api.vue
+++ pages/api.vue
@@ -2,23 +2,7 @@
   <div class="container api-container">
     <section class="section">
       <div class="content">
-        <h1 class="title is-medium">API Pustaka Kalender Jawa</h1>
-        <span class="api">cariHariPasaranAwalBulanTahunJawa(w,t)</span>
-        <div class="code-deskripsi">
-          <p>Metode ini digunakan untuk mencari hari dan pasaran untuk awal bulan pada tahun jawa tertentu.</p>
-          <p>Misalnya:</p>
-          <p>Untuk mencari hari dan pasaran awal bulan <strong>Romadon</strong> pada <strong>1952</strong> Tahun Jawa</p>
-        </div>
-        <pre v-highlightjs>
-         <code class="javascript">
-import * as KalenderJawa from '@junwatu/kalender-jawa'
-
-KalenderJawa.cariHariPasaranAwalBulanTahunJawa('romadon', 1952).then({kH, kP} => {
-  console.log(`${kH.dino} ${kP.pasaran}`) // senen legi
-})
-         </code>
-       </pre>
-       <HPAwalBulan/>
+        <HPAwalBulan/>
       </div>
     </section>
   </div>
