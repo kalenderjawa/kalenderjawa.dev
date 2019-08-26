c0e7f865fa95b01ffee0233c39473a07eac4c040
Equan Pr <equan.p@gmail.com>
2019-08-27 05:19:40 +0700

    Komponen dokumentasi

---
+++ pages/dokumentasi.vue
@@ -0,0 +1,276 @@
+<template>
+  <div class="container api-container">
+    <div class="content">
+      <h1 id="kalender-jawa">Kalender Jawa</h1>
+<h2 id="sejarah">Sejarah</h2>
+<p>Kalender atau Penanggalan Jawa merupakan penanggalan yang dibawa oleh para leluhur Jawa dan kemudian diolah lebih lanjut oleh para Wali. Sebelum masa para Wali, para leluhur Jawa telah memakai penanggalan berdasarkan matahari atau Saka dan juga berdasarkan bulan dan dalam perkembangan selanjutnya para Wali yang berkolaborasi dengan para leluhur bangsa Jawa tersebut menghasilkan Penanggalan Jawa yang baru tetapi berdasarkan pergerakan bulan dan Penanggalan Jawa baru ini sudah umum dipakai sejak Raden Patah menjadi Sultan Demak yang pertama. </p>
+<p>Momen dimana Raden Patah menjadi sultan pertama di Demak ini ditandai dengan <em>Sengkalan</em> atau <em>Candra Sengkala</em>,</p>
+<h3 id="-catur-ilang-sucining-ratu-"><em>Catur Ilang Sucining Ratu</em></h3>
+<p>Sengkalan tersebut menandakan tahun 1404 Saka (887 H, 1482 M) atau 4 tahun setelah runtuhnya Majapahit.</p>
+<blockquote>
+<p>Candra Sengkala atau Sengkalan merupakan cara orang Jawa dalam mengingat momen tahun sebagai suatu kalimat. Contoh paling terkenal adalah runtuhnya Majapahit yaitu &quot;Sirna ilang Kertaning Bumi&quot; atau tahun 1400 Jawa dan yang perlu menjadi catatan bahwa Sengkalan sebenarnya adalah kalender lunar yang sudah lama dipakai sebelum masa para wali [<a href="https://www.caknun.com/2019/kalender-jowo-digowo-kalender-arab-digarap-kalender-barat-diruwat">sumber</a>].</p>
+</blockquote>
+<p>Pada masa Sultan Agung terjadi peresmian Kalender Jawa sebagai kalender kerajaan yaitu Jumat Legi, 1 Sura 1555 (1 Muharram 1043 H, 8 Juli 1633 M, atau 51 tahun setelah kalender Gregorian diresmikan) yang berlaku untuk seluruh Pulau Jawa kecuali Banten, Blambangan dan Madura.</p>
+<blockquote>
+<p>Kalau dihitung-hitung dan dinalar sebenarnya para leluhur bangsa Indonesia jauh lebih cepat dalam perumusan dan pemakaian sistem kalender yaitu 100 tahun lebih awal dibandingkan Kalender Gregorian atau Masehi (peresmian Kalender Masehi adalah tahun 1582 M).</p>
+</blockquote>
+<h2 id="sistematis">Sistematis</h2>
+<p>Berikut aturan atau sistematis dari Kalender Jawa yang perlu menjadi acuan:</p>
+<ul>
+<li>Kalender Jawa Memakai sistem hari 7 <em>(saptawara)</em> yang sesuai dengan sistem hari pada kalender Masehi.</li>
+<li>Kalender Jawa mamakai sistem 5 <em>(pancawara)</em> atau sering dikenal dengan Pasaran.</li>
+<li>Kalender Jawa mempunyai siklus 8 tahunan (satu windu).</li>
+<li>Koreksi keberulangan terjadi setiap 120 tahun sekali (15 windu) atau istilahnya yaitu <strong>Salin Kurup</strong> atau <strong>Ganti Kurup</strong> (pengkoreksian yaitu dengan menambah 1 hari tiap 120 tahun).</li>
+</ul>
+<hr>
+<p>Beberapa sistematis berikut juga termasuk dalam Kalender Jawa hanya saja tidak (belum ada rencana) di implementasikan dalam <strong>Pustaka Kalender Jawa</strong> karena sifatnya yang pilihan:</p>
+<ul>
+<li>Sistem pembagian hari menjadi 6, <em>sadwara</em> atau <em>paringkelan</em>.</li>
+<li>Sistem siklus 210 hari atau 30 Wuku dimana tiap Wuku mempunyai siklus 7 hari. Sistem ini dikenal dengan nama Pawukon.</li>
+<li>Sistem <em>tumbuk</em> dimana setiap 4 windu sekali terjadi keberulangan nama hari, tanggal, pasaran dan bulan.</li>
+<li>Sistem <em>lambang</em> dengan siklus 2 windu.</li>
+</ul>
+<hr>
+<h3 id="hari-dinten-dino-">Hari (<em>Dinten</em>/<em>Dino</em>)</h3>
+<p>Sama seperti halnya sistem kalender Masehi, penanggalan Jawa juga memakai sistem pembagian hari menjadi 7.</p>
+<table>
+<thead>
+<tr>
+<th style="text-align:left">Urutan</th>
+<th style="text-align:left">Dinten</th>
+</tr>
+</thead>
+<tbody>
+<tr>
+<td style="text-align:left">1</td>
+<td style="text-align:left">Senen</td>
+</tr>
+<tr>
+<td style="text-align:left">2</td>
+<td style="text-align:left">Selasa</td>
+</tr>
+<tr>
+<td style="text-align:left">3</td>
+<td style="text-align:left">Rebo</td>
+</tr>
+<tr>
+<td style="text-align:left">4</td>
+<td style="text-align:left">Kemis</td>
+</tr>
+<tr>
+<td style="text-align:left">5</td>
+<td style="text-align:left">Jemah</td>
+</tr>
+<tr>
+<td style="text-align:left">6</td>
+<td style="text-align:left">Sebtu</td>
+</tr>
+<tr>
+<td style="text-align:left">7</td>
+<td style="text-align:left">Akad</td>
+</tr>
+</tbody>
+</table>
+<h3 id="pasaran">Pasaran</h3>
+<p>Selain sistem pembagian 7 hari seperti halnya pada penanggalan Masehi, masyarakat Jawa juga membagi hari menjadi 5 atau sering disebut dengan <strong>Pasaran</strong>. </p>
+<table>
+<thead>
+<tr>
+<th style="text-align:left">Pasaran</th>
+<th style="text-align:center">Neptu</th>
+</tr>
+</thead>
+<tbody>
+<tr>
+<td style="text-align:left">Legi</td>
+<td style="text-align:center">5</td>
+</tr>
+<tr>
+<td style="text-align:left">Pahing</td>
+<td style="text-align:center">9</td>
+</tr>
+<tr>
+<td style="text-align:left">Pon</td>
+<td style="text-align:center">7</td>
+</tr>
+<tr>
+<td style="text-align:left">Wage</td>
+<td style="text-align:center">4</td>
+</tr>
+<tr>
+<td style="text-align:left">Kliwon</td>
+<td style="text-align:center">8</td>
+</tr>
+</tbody>
+</table>
+<p><strong>Neptu</strong> adalah penilaian atau bobot angka dari suatu Pasaran. Asal mula dan bagaimana caranya menetapkan Neptu pada suatu Pasaran sejarahnya masih misteri, hanya saja penggunaanya sudah sangat umum sampai sekarang seperti penetapan acara adat dll. </p>
+<h3 id="bulan-sasi-">Bulan (<em>Sasi</em>)</h3>
+<p>Bulan atau <em>sasi</em> dalam Bahasa Jawa mempunyai jumlah sebanyak 12 bulan dengan nama-nama sebagai berikut: </p>
+<table>
+<thead>
+<tr>
+<th style="text-align:center">Urutan</th>
+<th style="text-align:left">Sasi</th>
+<th style="text-align:left">Alias</th>
+<th style="text-align:center">Jumlah Hari</th>
+</tr>
+</thead>
+<tbody>
+<tr>
+<td style="text-align:center">1</td>
+<td style="text-align:left">Mukarom</td>
+<td style="text-align:left">Suro</td>
+<td style="text-align:center">30</td>
+</tr>
+<tr>
+<td style="text-align:center">2</td>
+<td style="text-align:left">Sapar</td>
+<td style="text-align:left">-</td>
+<td style="text-align:center">29</td>
+</tr>
+<tr>
+<td style="text-align:center">3</td>
+<td style="text-align:left">Robi&#39;ulawal</td>
+<td style="text-align:left">Mulud</td>
+<td style="text-align:center">30</td>
+</tr>
+<tr>
+<td style="text-align:center">4</td>
+<td style="text-align:left">Robi&#39;ulakir</td>
+<td style="text-align:left">Bakda Mulud</td>
+<td style="text-align:center">29</td>
+</tr>
+<tr>
+<td style="text-align:center">5</td>
+<td style="text-align:left">Jumadilawal</td>
+<td style="text-align:left">-</td>
+<td style="text-align:center">30</td>
+</tr>
+<tr>
+<td style="text-align:center">6</td>
+<td style="text-align:left">Jumadilakir</td>
+<td style="text-align:left">-</td>
+<td style="text-align:center">29</td>
+</tr>
+<tr>
+<td style="text-align:center">7</td>
+<td style="text-align:left">Rojab</td>
+<td style="text-align:left">Rejeb</td>
+<td style="text-align:center">30</td>
+</tr>
+<tr>
+<td style="text-align:center">8</td>
+<td style="text-align:left">Sakban</td>
+<td style="text-align:left">Ruwah, Arwah</td>
+<td style="text-align:center">29</td>
+</tr>
+<tr>
+<td style="text-align:center">9</td>
+<td style="text-align:left">Romadon</td>
+<td style="text-align:left">Pasa, Ramelan, Puwasa</td>
+<td style="text-align:center">30</td>
+</tr>
+<tr>
+<td style="text-align:center">10</td>
+<td style="text-align:left">Sawal</td>
+<td style="text-align:left">-</td>
+<td style="text-align:center">29</td>
+</tr>
+<tr>
+<td style="text-align:center">11</td>
+<td style="text-align:left">Dulkodah</td>
+<td style="text-align:left">Séla, Dulkangidah, Apit</td>
+<td style="text-align:center">30</td>
+</tr>
+<tr>
+<td style="text-align:center">12</td>
+<td style="text-align:left">Dulkijah</td>
+<td style="text-align:left">Besar</td>
+<td style="text-align:center">29/30*</td>
+</tr>
+</tbody>
+</table>
+<p><strong>*) lihat Tabel Tahun Jawa</strong></p>
+<h3 id="tahun-jawa">Tahun Jawa</h3>
+<p>Di dalam Penanggalan Jawa, Tahun bukanlah satuan kalender yang terbesar tetapi dikenal yang namanya <strong>Windu</strong> yaitu 8 Tahun. Satuan windu ini dinamakan Tahun Jawa yang konsepnya tentu saja sangat berbeda dengan Tahun Masehi.</p>
+<p>Nama nama dan lama hari dari Tahun Jawa berada di <strong>Tabel Tahun Jawa</strong> berikut</p>
+<table>
+<thead>
+<tr>
+<th style="text-align:center">Urutan</th>
+<th style="text-align:left">Tahun Jawa</th>
+<th style="text-align:center">hari/tahun</th>
+</tr>
+</thead>
+<tbody>
+<tr>
+<td style="text-align:center">1</td>
+<td style="text-align:left">Alip</td>
+<td style="text-align:center">354</td>
+</tr>
+<tr>
+<td style="text-align:center">2</td>
+<td style="text-align:left">Ehe</td>
+<td style="text-align:center">355</td>
+</tr>
+<tr>
+<td style="text-align:center">3</td>
+<td style="text-align:left">Jimawal</td>
+<td style="text-align:center">354</td>
+</tr>
+<tr>
+<td style="text-align:center">4</td>
+<td style="text-align:left">Je</td>
+<td style="text-align:center">354</td>
+</tr>
+<tr>
+<td style="text-align:center">5</td>
+<td style="text-align:left">Dal</td>
+<td style="text-align:center">355</td>
+</tr>
+<tr>
+<td style="text-align:center">6</td>
+<td style="text-align:left">Be</td>
+<td style="text-align:center">354</td>
+</tr>
+<tr>
+<td style="text-align:center">7</td>
+<td style="text-align:left">Wawu</td>
+<td style="text-align:center">354</td>
+</tr>
+<tr>
+<td style="text-align:center">8</td>
+<td style="text-align:left">Jimakir</td>
+<td style="text-align:center">355</td>
+</tr>
+</tbody>
+</table>
+<p>dan dibawah ini adalah <strong>Tabel Hari Awal Tahun Kalender Jawa 1867‒2106 (1355‒1594 H, 1936‒2169 M)</strong></p>
+<p><img src="https://cdn.caknun.com/media/2019/01/20190102-menek-kalender-3.jpg" alt="Hari Awal Tahun Kalender Jawa 1867‒2106 (1355‒1594 H, 1936‒2169 M)"></p>
+<blockquote>
+<p>Tahun Jawa merupakan pembagian 8 dari siklus 120 tahun atau 8 x 15.  </p>
+</blockquote>
+<p>Seperti pada penjelasan sebelumnya tahun-tahun yang masuk di Tahun Jawa yang sama, misalnya tahun-tahun pada Tahun Be, maka semua tahun yang termasuk di Tahun Be tersebut akan mempunyai hari awal tahun yang sama yaitu <strong>Rebo Kliwon</strong> dan jumlah hari dalam tiap tahun juga sama yaitu sebanyak 354 hari.</p>
+<h2 id="rumus-matematis">Rumus Matematis</h2>
+<p>Kalender Jawa ini mempunyai keteraturan yang sangat luar biasa sehingga ditemukan pola-pola yang kemudian dirumuskan dan rumusan itu dengan mudah di buat menjadi suatu hafalan. Berdasarkan rumusan hari awal tahun kemudian didapatkan juga rumusan untuk hari-hari awal bulan pada masing-masing tahun dalam siklus windu.</p>
+<h3 id="rumus-hafalan-awal-bulan-kalender-jawa-abadi">Rumus Hafalan Awal Bulan Kalender Jawa Abadi</h3>
+<p> <img src="https://cdn.caknun.com/media/2019/01/20190102-menek-kalender-4.jpg" alt="rumus_awal_taun"></p>
+<p>Contoh, untuk menentukan hari awal bulan <strong>Tahun Baru Suro (Mukarom) 1953 J</strong> (1 Muharom 1441 H, 1 September 2019 M), langkahnya adalah sebagai berikut:</p>
+<ol>
+<li>Tahun <strong>1953</strong> termasuk <strong>Tahun Wawu, Kurup Asapon (Alip Selasa Pon)</strong>.</li>
+<li>Rumus awal dari bulan <strong>Mukarom</strong> Tahun <strong>Wawu</strong> yaitu <strong>&quot;Rom Nem Ro&quot;</strong> atau <strong>Mukarom Enem Loro</strong> (Mukarom 6 2). Angka depan 6 = hari, Angka belakang 2 = pasaran, pengaturan dihitung dari awal Kurup (Asapon).</li>
+<li>Hari dihitung mulai dari Selasa: 1. Selasa, 2. Rebo, 3. Kemis, 4. Jemah (Jum&#39;at) 5. Sebtu 6. Akad (Minggu). Hari ke-6 bertepatan dengan hari <strong>Akad</strong> atau <strong>Minggu</strong>.</li>
+<li>Pasaran dihitung mulai dari Pon: 1. Pon, 2. Wage. Pasaran ke-5 bertepatan dengan pasaran <strong>Wage</strong>.</li>
+<li>Sehingga, <strong>1 Suro (Mukarom) 1953 J</strong> bertepatan dengan hari <strong>Minggu Wage</strong>. </li>
+</ol>
+<hr>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  layout: 'isi'
+}
+</script>
+
