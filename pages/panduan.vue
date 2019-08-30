ad7166f2037b33d8675a0ac4e5fed53acb5f8a20
Equan Pr <equan.p@gmail.com>
2019-08-30 23:31:26 +0700

    Container isi panduan

---
+++ pages/panduan.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container api-container">
+    <div class="content">
+      <Wates/>
+    </div>
+  </div>
+</template>
+
+<script>
+import Wates from '~/components/Wates.vue'
+
+export default {
+  layout: 'dok',
+  components: {
+    Wates
+  }
+}
+</script>
