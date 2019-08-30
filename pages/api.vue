4ef27f6c35e37a1e11c4b9b850307b81651a5b5f
Equan Pr <equan.p@gmail.com>
2019-08-30 20:52:03 +0700

    Properti

--- pages/api.vue
+++ pages/api.vue
@@ -5,6 +5,41 @@
         <CKTJawa/>
         <CRumusAbadi/>
         <Sasi/>
+        <div>
+          <h2 id="properti">Properti</h2>
+<h3 id="-araningpasaran-"><code>araningPasaran</code></h3>
+<p>Nama atau <em>araning</em> dari Pasaran</p>
+<p> <strong>Return</strong>:</p>
+<ul>
+<li>Array</li>
+</ul>
+<p>Contoh</p>
+<pre><code class="lang-js"><span class="hljs-keyword">const</span> pasaran = KalenderJawa.araningPasaran
+console.<span class="hljs-built_in">log</span>(pasaran[<span class="hljs-number">0</span>].pasaran) <span class="hljs-comment">// legi</span>
+</code></pre>
+<h3 id="-araningdinten-"><code>araningDinten</code></h3>
+<p><strong>Return</strong></p>
+<ul>
+<li>Array</li>
+</ul>
+<p>Contoh</p>
+<pre><code class="lang-js">const iterator = KalenderJawa.araningDinten.<span class="hljs-built_in">values</span>()
+<span class="hljs-keyword">let</span> <span class="hljs-built_in">index</span> = iterator.<span class="hljs-keyword">next</span>()
+
+<span class="hljs-keyword">while</span> (!index.done) {
+  console.<span class="hljs-built_in">log</span>(<span class="hljs-built_in">index</span>.value) // { dino: <span class="hljs-string">'senen'</span>, urutan: <span class="hljs-number">1</span> } ...
+  <span class="hljs-built_in">index</span> = iterator.<span class="hljs-keyword">next</span>()
+}
+</code></pre>
+<h3 id="-taunjawa-"><code>taunJawa</code></h3>
+<p><strong>Return</strong></p>
+<ul>
+<li>Array</li>
+</ul>
+<p>Contoh </p>
+<pre><code class="lang-js">console.log(KalenderJawa<span class="hljs-selector-class">.tahunJawa</span>[<span class="hljs-number">1</span>]) <span class="hljs-comment">// { taun: 'dal', neptu: 4, urutan: 5, cacah: 355 }</span>
+</code></pre>
+        </div>
       </div>
   </div>
 </template>
