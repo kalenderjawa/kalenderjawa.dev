a4f977ca5cb458685cb9df782d4c4b4998ab533b
Equan Pr <equan.p@gmail.com>
2019-09-01 08:45:52 +0700

    Komponen Footer

---
+++ components/Footer.vue
@@ -0,0 +1,19 @@
+<template>
+  <footer class="footer">
+      <div class="content has-text-centered">
+        <p>
+          <span>Pustaka Kalender Jawa Abadi</span>
+          <br/>
+          © 2019, <a href="https://sajen.id" class="grad-link">Equan Pr</a> & Kolaborator
+          <br/>
+          Lisensi dari isi website <a href="http://creativecommons.org/licenses/by/4.0/">CC BY 4.0</a>
+        </p>
+      </div>
+  </footer>  
+</template>
+
+<script>
+export default {
+  name: 'Footer'
+}
+</script>
