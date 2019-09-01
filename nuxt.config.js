e0a2d8ef4a8bc021b28435c9dfeec22f83dd415e
Equan Pr <equan.p@gmail.com>
2019-09-01 08:22:15 +0700

    Set bahasa ke ID

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,6 +6,9 @@ export default {
   */
   head: {
     title: 'Pustaka Kalender Jawa',
+    htmlAttrs: {
+      lang: 'id'
+    },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
