e19d4cc8ad450e9bafc6e5fb4d8b65ceac559af5
Equan Pr <equan.p@gmail.com>
2019-09-02 08:08:51 +0700

    Komponen navigator utama

---
+++ components/Navin.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="navbar-menu is-active">
+        <div class="navbar-end">
+          <nuxt-link class="navbar-item" to="/">Home</nuxt-link>
+          <nuxt-link class="navbar-item" to="/api">API</nuxt-link>
+          <nuxt-link class="navbar-item" to="/panduan">Panduan</nuxt-link>
+          <nuxt-link class="navbar-item" to="/sejarah">Sejarah</nuxt-link>
+          <a class="navbar-item" href="https://github.com/kalenderjawa" alt="Kalender Jawa Github" target="_blank" rel="noopener"> 
+            <span class="icon is-large"><i class="fab fa-github"></i></span>
+          </a>
+        </div>
+      </div>
+</template>
+
+<script>
+export default {
+  name: 'Navin'
+}
+</script>
+
