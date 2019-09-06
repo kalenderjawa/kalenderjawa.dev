469ecd4903ce559b8d464f4fc063752b21225a75
Equan Pr <equan.p@gmail.com>
2019-09-06 14:53:57 +0700

    Codesandbox embed as component

--- pages/panduan.vue
+++ pages/panduan.vue
@@ -2,21 +2,20 @@
   <div class="container api-container">
     <div class="content">
       <Wates/>
-      <!-- codesandbox playground --> 
-      <h2>Online Editor</h2>
-      <p>Kalau anda tidak ingin ribet-ribet dengan instalasi anda bisa juga jika anda memakai editor online seperti Codesandbox.</p>
-      <iframe src="https://codesandbox.io/embed/distracted-glitter-rvl3z?autoresize=1&expanddevtools=1&fontsize=14" title="Pustaka Kalender Jawa " allow="geolocation; microphone; camera; midi; vr; accelerometer; gyroscope; payment; ambient-light-sensor; encrypted-media; usb" style="width:100%; height:500px; border:0; border-radius: 4px; overflow:hidden;" sandbox="allow-modals allow-forms allow-popups allow-scripts allow-same-origin"></iframe>
+      <EmbedCS/>
     </div>
   </div>
 </template>
 
 <script>
 import Wates from '~/components/Wates.vue'
+import EmbedCS from '~/components/EmbedCS.vue'
 
 export default {
   layout: 'dok',
   components: {
-    Wates
+    Wates,
+    EmbedCS
   },
   head () {
     return {
