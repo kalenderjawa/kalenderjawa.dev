2a8407ef4d4f62524fbd25b5c221bd57dd23c018
Equan Pr <equan.p@gmail.com>
2019-09-06 14:53:37 +0700

    EmbedCS component

---
+++ components/EmbedCS.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <!-- codesandbox playground --> 
+    <h2>Online Editor</h2>
+    <p>Kalau anda tidak ingin ribet-ribet dengan instalasi anda bisa juga jika anda memakai editor online seperti Codesandbox.</p>
+    <iframe src="https://codesandbox.io/embed/distracted-glitter-rvl3z?autoresize=1&expanddevtools=1&fontsize=14" title="Pustaka Kalender Jawa " allow="geolocation; microphone; camera; midi; vr; accelerometer; gyroscope; payment; ambient-light-sensor; encrypted-media; usb" style="width:100%; height:500px; border:0; border-radius: 4px; overflow:hidden;" sandbox="allow-modals allow-forms allow-popups allow-scripts allow-same-origin"></iframe>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'EmbedCS'
+}
+</script>
