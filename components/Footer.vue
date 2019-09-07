a13c0f7a643244de6d635185d040ab58086cea9b
Equan Pr <equan.p@gmail.com>
2019-09-07 07:14:37 +0700

    Add Slack icon & twittor

--- components/Footer.vue
+++ components/Footer.vue
@@ -2,9 +2,12 @@
   <footer class="footer">
       <div class="level">
         <div class="level-left">
-          <div class="level-item">
-            <a href="https://twitter.com/KalenderJawaDev" class="social grad-link">
-              <i>twitter</i>
+          <div class="level-item social">
+            <a href="https://twitter.com/KalenderJawaDev">          
+              <i class="fab fa-twitter fa-lg"></i>
+            </a>
+            <a href="https://join.slack.com/t/kalender-jawa/shared_invite/enQtNzM3MjA1OTM3OTg5LWExZTEyYTZhYWQ5NGRkOWU0NGZiZjk1Y2ZhY2M5N2E3NWUyYjI0Y2YwNDEyOTFiZTMwYTgxZjEyNzE1YjZiMzk">
+              <i class="fab fa-slack fa-lg"></i>
             </a>
           </div>
         </div>
