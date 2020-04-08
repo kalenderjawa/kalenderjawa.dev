eaffb9ce46ce0ead649a6240bffe815ce919e581
Equan Pr <equan.p@gmail.com>
2020-04-08 11:31:33 +0700

    Pake sitemap

--- nuxt.config.js
+++ nuxt.config.js
@@ -48,7 +48,14 @@ export default {
   */
   modules: [
     // Doc: https://github.com/nuxt-community/modules/tree/master/packages/bulma
-    '@nuxtjs/bulma'
+    '@nuxtjs/bulma',
+    [
+      '@nuxtjs/sitemap',
+      {
+        hostname: 'https://kalenderjawa.dev',
+        gzip: true
+      }
+    ]
   ],
   /*
   ** Build configuration
